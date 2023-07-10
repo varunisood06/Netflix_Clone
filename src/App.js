62d809cd8c94cf3ad875238108317933fcb17a80
varunisood06 <119521409+varunisood06@users.noreply.github.com>
2023-07-10 18:51:11 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Counter } from './features/counter/Counter';
+import './App.css';
+import './index.css';
+import HomeScreen from './HomeScreen';
+
+
+function App() {
+  return (
+    <div className="app">
+     <HomeScreen/>
+    </div>
+  );
+}
+
+export default App;
